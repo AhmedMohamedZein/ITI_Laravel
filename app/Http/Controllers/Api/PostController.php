90e8b7292faacdf8954c67f243e0f1f6e33c81b0
AhmedMohamedZein <ahmedzain503@gmail.com>
2023-04-01 13:42:46 +0200

    Adding the post api

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\UpdatePostRequest;
 use App\Http\Resources\PostResource;
+use App\Http\Requests\StorePostRequest;
 use Illuminate\Http\Request;
 use App\Models\Post;
 class PostController extends Controller
@@ -14,7 +15,7 @@ class PostController extends Controller
         return PostResource::collection($posts);
     }
 
-    public function show ($id) { //Model Pinding
+    public function show ($id) {
         $post = Post::find($id);
         if ( !empty($post) ) {
             return new PostResource($post);
@@ -23,14 +24,42 @@ class PostController extends Controller
             return response()->json(['message' => 'Post not found'], 404);
         }
     }
+    public function store (StorePostRequest $request) {
 
-    public function update ($id , UpdatePostRequest $request) {
-
-        $post = Post::find($id);
-        $post->update([
+        $post = Post::create([
             'title' => $request->title,
             'description' => $request->description,
-            'content' =>  $request->content
+            'content' =>  $request->content,
+            'user_id' => $request->user_id
         ]);
+
+        if (isset($request['img_src'])) {
+            $post->img_src = $request['img_src'];
+        }
+
+        return new PostResource($post);
+    }
+    public function update ($id  , UpdatePostRequest $request) {
+        $post = Post::find($id);
+        if ( !empty($post) ) {
+            $post->update([
+                'title' => $request->title,
+                'description' => $request->description,
+                'content' =>  $request->content
+            ]);
+            return new PostResource($post);
+        }else {
+            return response()->json(['message' => 'Post not found'], 404);
+        }
+    }
+
+    public function destroy ($id) {
+        $post = Post::find($id);
+        if ( !empty($post) ) {
+            $post->delete();
+            return response()->json(['message' => 'Deleted'], 200);
+        }else {
+            return response()->json(['message' => 'Post not found'], 404);
+        }
     }
 }
